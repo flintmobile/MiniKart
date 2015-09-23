b7b51988b3e28d6a90461600924811faae0fe5ed
PC Nguyen <pc@flint.com>
2015-09-23 14:46:57 -0700

    adding pod file. swipe table, change payment flow initiation

--- MiniKart/MasterViewController.swift
+++ MiniKart/MasterViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FlintConnectSDK
+import SESlideTableViewCell
 
 class MasterViewController: UITableViewController {
 
@@ -15,13 +16,20 @@ class MasterViewController: UITableViewController {
   var objects = [AnyObject]()
   var orderItems = [FlintOrderItem]()
 
+  override func loadView() {
+    super.loadView()
+    
+    self.tableView.rowHeight = 60
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
+    
+    let payButton = UIBarButtonItem(title: "Pay", style: .Plain, target: self, action: "takePayment:")
     let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
     self.navigationItem.rightBarButtonItem = addButton
+    self.navigationItem.leftBarButtonItem = payButton
+    
     if let split = self.splitViewController {
         let controllers = split.viewControllers
         self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -31,30 +39,6 @@ class MasterViewController: UITableViewController {
   override func viewWillAppear(animated: Bool) {
     self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
     super.viewWillAppear(animated)
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  func insertNewObject(sender: AnyObject) {
-    orderItems.removeAll()
-    splitViewController?.toggleMasterView()
-    
-    // create mock order
-    let orderItem = FlintOrderItem()
-    orderItem.name = "Preset Item"
-    orderItem.quantity = 1
-    orderItem.price = 15
-    orderItem.taxAmount = 3
-    orderItems.append(orderItem)
-    
-    if let paymentViewController = FlintUI.paymentViewControllerWithOrderItems(orderItems, delegate: self) {
-      let navigationController = UINavigationController(rootViewController: paymentViewController)
-      navigationController.modalPresentationStyle = .FormSheet
-      splitViewController?.presentViewController(navigationController, animated: true, completion: nil)
-    }
   }
 
   // MARK: - Segues
@@ -82,15 +66,16 @@ class MasterViewController: UITableViewController {
   }
 
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! SESlideTableViewCell
 
     let object = objects[indexPath.row] as! NSDate
     cell.textLabel!.text = object.description
+    cell.addLeftButtonWithText("  +   ", textColor: UIColor.whiteColor(), backgroundColor: UIColor.greenColor())
+    cell.addLeftButtonWithText(" -    ", textColor: UIColor.whiteColor(), backgroundColor: UIColor.redColor())
     return cell
   }
 
   override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
     return true
   }
 
@@ -100,6 +85,33 @@ class MasterViewController: UITableViewController {
         tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
     } else if editingStyle == .Insert {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    }
+  }
+  
+  // MARK : - Actions
+  
+  func insertNewObject(sender: AnyObject) {
+    objects.insert(NSDate(), atIndex: 0)
+    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+  }
+  
+  func takePayment(sender: AnyObject) {
+    orderItems.removeAll()
+    splitViewController?.toggleMasterView()
+
+    // create mock order
+    let orderItem = FlintOrderItem()
+    orderItem.name = "Preset Item"
+    orderItem.quantity = 1
+    orderItem.price = 15
+    orderItem.taxAmount = 3
+    orderItems.append(orderItem)
+
+    if let paymentViewController = FlintUI.paymentViewControllerWithOrderItems(orderItems, delegate: self) {
+      let navigationController = UINavigationController(rootViewController: paymentViewController)
+      navigationController.modalPresentationStyle = .FormSheet
+      splitViewController?.presentViewController(navigationController, animated: true, completion: nil)
     }
   }
 }
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+use_frameworks!
+
+target 'MiniKart' do
+pod 'SESlideTableViewCell'
+end
+
+target 'MiniKartTests' do
+
+end
+
+target 'MiniKartUITests' do
+
+end
+
