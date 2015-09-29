5c113b5d6c294371e33d7960cf645f8cc6c4f5fa
PC Nguyen <pc@flint.com>
2015-09-29 12:35:44 -0700

    adding the gallery

--- MiniKart/DetailViewController.swift
+++ MiniKart/DetailViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-  @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+  @IBOutlet weak var collectionView: UICollectionView!
+  var collectionViewSize: CGSize?
+  
   var detailItem: MenuItem? {
     didSet {
         // Update the view.
@@ -21,25 +21,65 @@ class DetailViewController: UIViewController {
   }
 
   func configureView() {
-    // Update the user interface for the detail item.
-    if let detail = self.detailItem {
-        if let label = self.detailDescriptionLabel {
-            label.text = detail.name
-        }
-    }
+    
   }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
     self.configureView()
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+    super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+    collectionViewSize = size
+    let oldFrame = collectionView.frame
+    collectionView.frame = CGRectMake(CGRectGetMinX(oldFrame), CGRectGetMinY(oldFrame), size.width, size.height)
+    collectionView.collectionViewLayout.invalidateLayout()
   }
+}
 
+// MARK: - Extensions
 
+extension DetailViewController : UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+
+  func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 20
+  }
+  
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("GalleryCell", forIndexPath: indexPath) as! GalleryCell
+    
+    cell.imageView.image = UIImage(named: "Gallery\(indexPath.item + 1)")
+    return cell
+  }
+  
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    
+    let actualSize: CGSize = collectionView.bounds.size
+    
+    let width: CGFloat
+    let height: CGFloat
+    if self.traitCollection.userInterfaceIdiom == .Pad {
+      width = actualSize.width / 5
+      height = actualSize.height / 4
+    } else {
+      width = actualSize.width / 2
+      height = actualSize.height / 3
+    }
+    
+    return CGSizeMake(width, height)
+  }
+  
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+    return 0
+  }
+  
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+    return 0
+  }
 }
 
---
+++ MiniKart/GalleryCell.swift
@@ -0,0 +1,15 @@
+//
+//  GalleryCell.swift
+//  MiniKart
+//
+//  Created by PC on 9/29/15.
+//  Copyright © 2015 Flint. All rights reserved.
+//
+
+import UIKit
+
+class GalleryCell: UICollectionViewCell {
+
+  @IBOutlet weak var imageView: UIImageView!
+  
+}
